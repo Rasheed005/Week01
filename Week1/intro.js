74c69aee9f1dbc0b7d4e51e3c6da11f1521a6e34
Rashcode <saudahanuoluwapo@gmail.com>
2026-02-14 15:29:03 +0100

    first

---
+++ Week1/intro.js
@@ -0,0 +1,7 @@
+let myName = "RASHEED SAUDAH";
+let age = 25;
+let favorite = "JavaSript";
+
+console.log(`My name is ${myName}`);
+console.log(` I am ${age} yrs old`);
+console.log(`My favorite programming language is  ${favorite}`);
